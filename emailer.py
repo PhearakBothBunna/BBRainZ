02c2f0e81c0e48ccbe291ee31797bc59479c6516
ZaynAbou-Harb <75918558+ZaynAbou-Harb@users.noreply.github.com>
2022-02-19 18:57:09 -0800

    Add files via upload

--- emailer.py
+++ emailer.py
@@ -1,17 +1,21 @@
 import smtplib
 import sys
+import imghdr
 from email.message import EmailMessage
 from email.utils import make_msgid
 
 #Sends an email with the attached image
-#argv[1] - email sender name
-#argv[2] - email sender address
-#argv[2] - email recipient address
+#argv[1] - student name
+#argv[2] - student ID
+#argv[3] - student address
+#argv[4] - advisor address
+
+FILE_NAME = 'schedule.jpg'
 
 sender_address = "bbrainzproject@gmail.com"
-recipient_address = str(sys.argv[3]) + ", " + str(sys.argv[2])
+recipient_address = str(sys.argv[4]) + ", " + str(sys.argv[3])
 attachment_name = str(sys.argv[1]) + "'s Schedule"
-
+student_id = str(sys.argv[2])
 
 msg = EmailMessage()
 msg['To'] = recipient_address
@@ -24,15 +28,19 @@ msg.set_content("""\
 <!DOCTYPE html>
 <html>
   <h1>%s</h1>
+  <h2>%s</h2>
   <body>
     <img src='cid:%s' alt="Schedule">
   </body>
 </html>
-""" % (attachment_name, attachment_cid[1:-1]), 'html')
+""" % (attachment_name, student_id, attachment_cid[1:-1]), 'html')
 
-with open('schedule.jpg', 'rb') as img:
+with open(FILE_NAME, 'rb') as img:
+  file_data = img.read()
+  file_type = imghdr.what(img.name)
+
   msg.add_related(
-    img.read(), 'image', 'jpeg', cid=attachment_cid)
+    file_data, 'image', file_type, cid=attachment_cid)
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
   smtp.login(sender_address, 'sktelxvlvclhfblx')
