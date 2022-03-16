03b29f4962fde088a5eaea4579231a5f968df7ba
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2022-03-16 00:44:56 -0700

    Update fecthingHtml.py

--- fetching/fecthingHtml.py
+++ fetching/fecthingHtml.py
@@ -2,37 +2,6 @@ from itertools import count
 import requests
 import json
 
-def toFile(list):
-    for x in list:
-        with open('html3.txt', 'a') as f:
-            f.write(getCourses(x))
-
-def listToString(list, str1):
-    return (str1.join(list))
-
-def filter(list):
-    res = []
-    for x in list:
-        if "%" in x:
-            res.append(x)
-    return res
-
-def toJson(coursesId, prereq):
-    res = []
-    for x in range(len(coursesId)):
-        list.append({coursesId[x] : prereq[x]})
-    res = json.dumps(list)
-    return res
-
-def divide(list):
-    coursesId = []
-    prereq = []
-    for x in list:
-        cur = x.split("%")
-        coursesId.append(cur[0])
-        prereq.append(cur[1])
-    return coursesId, prereq
-
 def getCourses(str):
     # create a txt file that holds the courses
     with open('courses.txt', 'w') as f:
@@ -53,14 +22,23 @@ def getCourses(str):
         x = x.replace('</div>','')
         x = x.replace('<p class="course_ending">','')
         x = x.replace('<span class="course_data">', '')
-        course = x.split('Course Prerequisite:')
         # only keep the courses that are under 500 level
-        if len(course) > 1:
-            courses.append(course)
-    for course in courses:
-        course[1] = course[1].split('.')[0]
-        with open('courses.txt', 'a') as f:
-            f.write(course[0] + "%" + course[1] + "\n")
+        if int(x[0:3]) < 500:
+            if x[4] == '[':
+                end = 4
+                while x[end] != ']':
+                    end = end + 1
+                x = x.replace(x[4:end + 1], '')
+            if "Course Prerequisite:" in x:
+                temp = x.split("Course Prerequisite:")
+                keep = temp[0][0:3]
+                for i in range(3, (len(temp[0]))):
+                    if not (temp[0][i].isnumeric() or temp[0][i] == '(' or temp[0][i] == ')' or temp[0][i] == '-'):
+                        keep = keep + temp[0][i]
+                x = keep + "Course Prerequisite:" + temp[1]
+            courses.append(x)
+            with open('courses.txt', 'a') as f:
+                f.write(x + "\n")
     
 # fetching the html file from wsu cs major
 fetching = requests.get('https://catalog.wsu.edu/Pullman/Courses/BySubject/CPT_S')
